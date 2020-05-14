07e1ab5d86278c92e1528fbbc22e497a7648f699
Manny <emanual4real@gmail.com>
2020-05-13 23:28:23 -0400

    wip

--- src/pages/Books/TableFooter.jsx
+++ src/pages/Books/TableFooter.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useReducer } from "react";
 // import PropTypes from "prop-types";
+import { Button } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 
+function reducer(state, { field, value }) {
+  return {
+    ...state,
+    [field]: value,
+  };
+}
+
 function BookFooter(props) {
   const { header } = props;
+  // create initial state
+  const initialState = {};
+  header.forEach((row) => (initialState[row.name] = ""));
+  // store initial state in reducer
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <tr key={`tr-new-book`} className="d-flex">
@@ -15,9 +28,23 @@ function BookFooter(props) {
           return (
             <td key={`td-${col.name}`} className="col">
               {col.name === "edit" ? (
-                <FontAwesomeIcon icon={faFolderPlus} />
+                <button
+                  className="btn"
+                  style={{ backgroundColor: "transparent" }}
+                  onClick={(e) => console.log("state", state)}
+                >
+                  <FontAwesomeIcon icon={faFolderPlus} />
+                </button>
               ) : (
-                <input type="text" style={{ width: "100%" }}></input>
+                <input
+                  type="text"
+                  value={state[col.name]}
+                  name={col.name}
+                  onChange={(e) =>
+                    dispatch({ field: e.target.name, value: e.target.value })
+                  }
+                  style={{ width: "100%" }}
+                ></input>
               )}
             </td>
           );
